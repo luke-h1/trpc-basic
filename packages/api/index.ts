1eb37a187a618652e297f9a8939afa0794e4d7d3
luke-h1 <luke.howsam@yahoo.com>
2022-03-12 16:12:32 +0000

    hello world

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,6 +1,22 @@
 import express from "express";
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+
+const appRouter = trpc.router().query("hello", {
+  resolve() {
+    return "Hello TRPC world";
+  },
+});
 
 const app = express();
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null,
+    batching: { enabled: true },
+  })
+);
 const port = 8080;
 
 app.get("/", (req, res) => {
